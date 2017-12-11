83998031131200b7961e39f4d8401dd6553ed1d3
Tamara von Glehn <tamara.vonglehn@gmail.com>
2017-12-11 18:08:13 +0000

    added coq-prog-args

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,10 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-(("" (proof-prog-name-ask . true)))
+((coq-mode
+  . ((eval . 
+	   (let
+	     (make-local-variable 'coq-prog-args)
+	     (setq coq-prog-args `("-emacs" "-indices-matter" "-type-in-type" ))
+	     (make-local-variable 'proof-prog-name-ask)
+	     (setq proof-prog-name-ask `1))))))
