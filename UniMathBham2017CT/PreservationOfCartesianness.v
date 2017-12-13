ede9640423740a54a2e45626c055ccab297d6fec
Tamara von Glehn <tamara.vonglehn@gmail.com>
2017-12-13 18:04:44 +0000

    defined homset isomorphism in displayed adjunction

--- UniMathBham2017CT/PreservationOfCartesianness.v
+++ UniMathBham2017CT/PreservationOfCartesianness.v
@@ -11,21 +11,7 @@ Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
 Require Import UniMath.CategoryTheory.DisplayedCats.Fibrations.
 
 Require Import UniMath.CategoryTheory.DisplayedCats.Equivalences_bis.
-
-
-Definition preserves_cartesianness
-           {C C' : category}
-           {F : functor C C'}
-           {D} {D'}
-           (FF : disp_functor F D D') : UU
-  :=
-    ∏ (c c' : C) (f : C⟦c, c'⟧)
-      (d : D c) (d' : D c')
-      (ff : d -->[f] d'),
-    is_cartesian ff -> is_cartesian (#FF ff).
-
-
-
+Require Import TypeTheory.Displayed_Cats.ComprehensionC.
 
 Section fix_disp_adjunction.
 
@@ -34,85 +20,125 @@ Context {C C' : category}
         {D : disp_cat C}
         {D': disp_cat C'}
         (X : disp_adjunction A D D').
-Let F : functor _ _ := left_adjoint A.
-Let G : functor _ _ := right_adjoint A.
+Let F := left_functor A.
+Let G := right_functor A.
 
-Let FF : disp_functor _ D D' := left_adj_over X.
-Let GG : disp_functor _ D' D := right_adj_over X.
+Let FF : disp_functor F D D' := left_adj_over X.
+Let GG : disp_functor G D' D := right_adj_over X.
 
 Section DispHomSetIso_from_Adjunction.
 
-  Let η := unit_from_are_adjoints A.
-  Let ε := counit_from_are_adjoints A.
+  Let η : functor_identity C ⟹ F ∙ G := adjunit A.
+  Let ε : G ∙ F ⟹ functor_identity C' := adjcounit A.
+  Let ηη : disp_nat_trans η (disp_functor_identity D) (disp_functor_composite FF GG)
+    := unit_over X.
+  Let εε : disp_nat_trans ε (disp_functor_composite GG FF) (disp_functor_identity D')
+    := counit_over X.
 
   Definition homset_conj_inv {c : C} {c' : C'} (g : C⟦c, G c'⟧)
              (d : D c) (d' : D' c') :
-      (d -->[g] GG _ d') -> (FF _ d -->[#F g · adjcounit A _] d').
+      (d -->[g] GG _ d') -> (FF _ d -->[#F g ·  ε _] d').
   Proof.
   intro alpha.
-  set (FFalpha := disp_functor_on_morphisms FF alpha).
-  exact (comp_disp FFalpha (counit_over X _ _)).
+  exact (comp_disp (# FF alpha) (εε _ _)).
   Defined.
-
-  Definition homset_conj {c : C} {c' : C'} (f : C'⟦F c, c'⟧)
-             (d : D c) (d' : D' c') :
-      (FF _ d -->[f] d') -> (d -->[adjunit A _ · #G f] GG _ d').
-  Proof.
+  
+  Definition homset_conj {c : C} {c' : C'} (g : C⟦c, G c'⟧)
+  (d : D c) (d' : D' c') : 
+     (FF _ d -->[#F g ·  ε _] d') -> (d -->[g] GG _ d'). 
+  Proof. 
   intro beta.
-  set (GGbeta := disp_functor_on_morphisms GG beta).
-  exact (comp_disp (unit_over X _ _) GGbeta).
+  set (gamma := comp_disp (ηη _ _) (# GG beta)). 
+  simpl in gamma.
+  (*
+  assert (equiv : η c · # G (# F g · ε c') = g) by
+      exact (φ_adj_after_φ_adj_inv A g).
+  exact (transportf _ equiv gamma). *)
+  exact (transportf _ (φ_adj_after_φ_adj_inv A g) gamma).
   Defined.
 
-  Lemma homset_conj_inv_after_conj {c : C} {c' : C'} (f : C'⟦F c, c'⟧) (d : D c) (d' : D' c') (beta : FF _ d -->[f] d') : transportf _ (φ_adj_inv_after_φ_adj A f) (homset_conj_inv _ d d' (homset_conj f d d' beta)) = beta.
-  unfold homset_conj.
-  unfold homset_conj_inv.
-(* TODO: finish this proof, assuming the lemma is true! *)
-  unfold φ_adj_inv_after_φ_adj.
-
-  (* simpl in gamma. *)
-  (* assert (equiv : (adjunit A) c · # (right_functor A) (# F g · (adjcounit A) c') = g). *)
-  (* apply (φ_adj_after_φ_adj_inv A g). *)
-  (* exact (transportf _ equiv gamma). *)
-  (* Defined. *)
-
-  (* Definition homset_conjinv {c : C} {c' : C'} (g : C⟦c, G c'⟧) *)
-  (*            (d : D c) (d' : D' c') : *)
-  (*     (FF _ d -->[#F g · adjcounit A _] d') -> (d -->[g] GG _ d'). *)
+  Lemma homset_conj_inv_after_conj {c : C} {c' : C'} (g : C⟦c, G c'⟧)
+        (d : D c) (d' : D' c')
+        (beta : FF _ d -->[#F g · ε _] d') :
+    homset_conj_inv _ _ _ (homset_conj g d d' beta) = beta. 
+  Proof.
+    Open Scope mor_disp.
+    unfold homset_conj.
+    unfold homset_conj_inv.
+    rewrite (disp_functor_transportf).
+    Check (# FF (ηη c d;; # GG beta)).
+  Admitted.
+    (* Stuck! Error: Found no subterm matching "# FF (ηη c d;; # GG beta)" in the current goal. *)
+    (* Old attempts: 
+    rewrite (disp_functor_comp FF (ηη c d) (# GG beta)). 
+    assert (S: # FF (ηη c d;; # GG beta) = transportb _ (functor_comp F _ _) (# FF (ηη c d);;# FF (# GG beta))).
+    apply (disp_functor_comp).
+    rewrite S.
+    rewrite (mor_disp_transportf_postwhisker).
+    simpl.
+    unfold transportb.
+    rewrite (mor_disp_transportf_postwhisker).
+    rewrite (assoc_disp_var).
+    simpl.
+    assert (S : # FF (# GG beta) = (# (disp_functor_composite GG FF) beta))
+      by (apply idpath).
+    rewrite S.
+    rewrite (disp_nat_trans_ax εε beta).
+    assert ((# FF (ηη c d);; (# FF (# GG beta);; εε c' d'))  = ((# FF (ηη c d);; (# (disp_functor_composite GG FF) beta) ;;  εε c' d'))). 
+    apply idpath.
+    Check (# FF (# GG beta)).
+    unfold comp_disp.
+    rewrite X0.
+    rewrite (disp_nat_trans_ax εε beta). *)
+    
+(* Alternative symmetric definition : *)
+    (* Definition homset_conj {c : C} {c' : C'} (f : C'⟦F c, c'⟧) *)
+  (*           (d : D c) (d' : D' c') : *)
+  (*    (FF _ d -->[f] d') -> (d -->[adjunit A _ · #G f] GG _ d'). *)
   (* Proof. *)
   (* intro beta. *)
   (* set (GGbeta := disp_functor_on_morphisms GG beta). *)
-  (* set (gamma := comp_disp (unit_over X _ _) GGbeta). *)
-  (* simpl in gamma. *)
-  (* assert (equiv : (adjunit A) c · # (right_functor A) (# F g · (adjcounit A) c') = g). *)
-  (* apply (φ_adj_after_φ_adj_inv A g). *)
-  (* exact (transportf _ equiv gamma). *)
+  (* exact (comp_disp (unit_over X _ _) GGbeta). *)
   (* Defined. *)
 
-  
-    
-  Lemma dispadjunction_hom_weq (c : C) (c' : C') (g : C⟦c, G c'⟧) (d : D c) (d' : D' c') :
-      (d -->[g] GG _ d') ≃ (FF _ d -->[#F g · adjcounit A _] d').
+  Lemma homset_conj_after_conj_inv {c : C} {c' : C'} (g : C⟦c, G c'⟧)
+        (d : D c) (d' : D' c')
+        (alpha : d -->[g] GG _ d') :
+    homset_conj _ _ _ (homset_conj_inv g d d' alpha) = alpha. 
   Proof.
+    unfold homset_conj.    
+    unfold homset_conj_inv.
+    rewrite disp_functor_comp.
+    simpl.
+    unfold transportb.
+    rewrite (mor_disp_transportf_prewhisker).
+    rewrite (assoc_disp).
+    (* Stuck again *)
+    (* rewrite (disp_nat_trans_ax_var ηη alpha). *)
+  Admitted.  
 
-
-  Definition φ_adj {A : C} {B : D} : F A --> B → A --> G B
-    := λ f : F A --> B, η _ · #G f.
-
-  Definition φ_adj_inv {A : C} {B : D} : A --> G B → F A --> B
-    := λ g : A --> G B, #F g · ε _ .
+  Close Scope mor_disp.
   
-  
+  Lemma dispadjunction_hom_weq (c : C) (c' : C') (g : C⟦c, G c'⟧) (d : D c) (d' : D' c') :
+      (d -->[g] GG _ d') ≃ (FF _ d -->[# F g · adjcounit A _] d').
+  Proof.
+    exists (homset_conj_inv g d d').
+    apply (gradth _ (homset_conj g d d')).  
+    - apply homset_conj_after_conj_inv.  
+    - apply homset_conj_inv_after_conj.
+  Defined.
+
 End DispHomSetIso_from_Adjunction.
 
 
-
-Lemma right_over_adj_preserves_cartesianness : preserves_cartesianness GG.
+Lemma right_over_adj_preserves_cartesianness : is_cartesian_disp_functor GG.
 Proof.
-  unfold preserves_cartesianness.
+  unfold is_cartesian_disp_functor.
   intros c c' f d d' ff ff_cart.
   intros c'' g d'' h.
   unfold is_cartesian in ff_cart.
-
+  eapply iscontrweqb. 
+    
   (** Path to happiness:
 
 - construct homset-isomorphisms defined by a displayed adjunction;
